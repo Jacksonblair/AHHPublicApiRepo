ca79f12b82b95c28997c602c8fd072a53570f4b3
Jacksonblair <jackson.blair@live.com>
2021-06-21 17:32:40 +1000

    where did i learn to program

--- api/endpoints/util/getNeedMetaTags.js
+++ api/endpoints/util/getNeedMetaTags.js
@@ -4,7 +4,7 @@ module.exports = (url, need) => { `
 		<head>
 		    <meta property="og:url"                content="${url}" />
 		    <meta property="og:type"               content="article" />
-		    <meta property="og:title"              content="${need.name}" />
+		    <meta property="og:title"              content="${need.title}" />
 		    <meta property="og:description"        content="${need.details}" />
 		    <meta property="og:image"              content="${need.image_url}" />
 		</head>
