cc6057cd81b7a1551c5b8ae3c3e8b950a364c70a
Jacksonblair <jackson.blair@live.com>
2021-06-21 17:42:04 +1000

    fix

--- api/endpoints/util/getNeedMetaTags.js
+++ api/endpoints/util/getNeedMetaTags.js
@@ -1,11 +1,15 @@
 module.exports = (url, need) => {
+	console.log(url)
+	console.log(need)
+
 	return `<html>
 		<head>
 		    <meta property="og:url"                content="${url}" />
 		    <meta property="og:type"               content="article" />
 		    <meta property="og:title"              content="${need.name}" />
 		    <meta property="og:description"        content="${need.details}" />
-		    <meta property="og:image"              content="${need.image_url}" />
+		    <meta property="og:image"              content="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" />
+		    <meta property="fb:app_id"             content="${process.env.FB_APP_ID}" />
 		</head>
 		<body>
 		</body>
